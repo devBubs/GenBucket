8df2df96e3ef4911d8cd70c8dd39fc0329688cbc
Naveen <navkmn@amazon.com>
2020-09-23 10:31:09 +0530

    initial commit

---
+++ src/main/java/c2/elastic/bucket/GenBucket/GenBucketApplication.java
@@ -0,0 +1,13 @@
+package c2.elastic.bucket.GenBucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenBucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenBucketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/c2/elastic/bucket/GenBucket/GenBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package c2.elastic.bucket.GenBucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenBucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
